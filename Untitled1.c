d306ca1909ef9f7bfb2ba47f7315444a10a6baf0
Tiago <tiagotts2026@gmail.com>
2026-03-24 13:26:17 -0300

    Calculadora versão 0

---
+++ Untitled1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+
+	float numero1, numero2, resultado;
+	char operacao;
+	
+	printf("Digite o numero que voce quer operar: ");
+	scanf("%f",&numero1);
+	printf("Escolha a operacao entre +, -, *, /: ");
+	scanf(" %c",&operacao);
+	printf("Digite outro numero: ");
+	scanf("%f",&numero2);
+	
+	if (operacao == '+'){
+		resultado = numero1 + numero2;
+}	else if (operacao == '-'){
+		resultado = numero1 - numero2;
+}	else if (operacao == '*'){
+		resultado = numero1 * numero2;
+}	else if (operacao == '/'){
+		resultado = numero1 / numero2;
+		
+}	
+
+			
+	
+	printf("O resultado da operacao eh %.2f",resultado);
+
+	return 0;
+}
